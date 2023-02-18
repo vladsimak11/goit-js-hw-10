ac3f8bb4e633924be32d5bbaf0d73687a492954b
Vladyslav Simak <simakv21@gmail.com>
2023-02-18 16:56:05 +0200

    Code done!

--- src/index.js
+++ src/index.js
@@ -1,29 +1,78 @@
 import './css/styles.css';
 import API from './fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
+const countryInfo = document.querySelector('.country-info');
 
-searchBox.addEventListener('input', debounce(onSearchCountry, 1000));
-
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
+searchBox.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
 
 function onSearchCountry(event) {
 
   event.preventDefault();
   const inputValue = event.target.value;
+  const inputLength = event.target.value.length;
 
   API.fetchCountries(inputValue)
   .then(country => {
-    console.log(country);
+    
+    if(country.length > 10) {
+      return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    } 
+    
+    if(country.length >= 2 && country.length <= 10) {
+      return renderListCountry(country);
+    } 
+    
+    return render.renderCardCountry(country);
+
   })
-  .catch(error => {
-    console.log(error);
+  .catch(() => {
+    Notiflix.Notify.failure('Oops, there is no country with that name');;
   })
 }
 
+function renderListCountry(countryValues) {
+  const markup = countryValues
+    .map(({name, flags}) => {
+      return `
+      <li class="main-info">
+        <img src="${flags.svg}"
+        alt="Flag"
+        />
+        <p class="name-country"> ${name.official}</p>
+      </li>
+      `;
+    })
+    .join("");
 
+  countryInfo.innerHTML = markup;
+}
 
+function renderCardCountry(countryValues) {
+  const markup = countryValues
+    .map(({name, capital, population, flags, languages}) => {
+      return `
+      <div class="main-info">
+        <img src="${flags.svg}"
+        alt="Flag"
+        />
+        <p class="name"> ${name.official}</p>
+      </div>
 
+      <div>
+        <p><b>Capital</b>: ${capital}</p>
+        <p><b>Population</b>: ${population}</p>
+        <p><b>Languages</b>: ${Object.values(languages)}</p>
+      </div>
+      `;
+    })
+    .join("");
+
+  countryInfo.innerHTML = markup;
+}
+
+export default { renderCardCountry};
